0c3762de4d849e4b59bfb2a73cdbc397e4e7d759
Encek <pawel112233@gmail.com>
2021-02-18 13:17:12 +0100

    update

--- pull.ps1
+++
@@ -1,4 +0,0 @@
-Write-Host -NoNewLine 'Pull world and replace old one?';
-$null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
-git pull
-cp .\NewHope.db C:\users\ja\AppData\LocalLow\IronGate\Valheim\worlds\
